dd73a580638bc4f71031f4bce07a2516425b97f4
Thomas Getgood <thomas.getgood@gmail.com>
2015-05-17 00:11:51 +0200

    Attempting to clean up.

--- src/gmaps/core.cljs
+++ src/gmaps/core.cljs
@@ -1,5 +1,6 @@
 (ns gmaps.core
   (:require [gmaps.location :as loc]
+            [gmaps.map.markers :as markers]
             [gmaps.map.directions :as dir]))
 
 (def maps (atom {}))
@@ -17,18 +18,29 @@
            (into {} (filter (fn [[k v]] (contains? init-keys k)) map-data)))))
 
 (defn- update-map*
-  [{:keys [map-obj map-data]} new-data]
-  (when-not (= (-> map-data :directions) (-> new-data :directions))
-    (dir/set-directions! map-obj (:directions new-data)))
-  (when-not (loc/same-place? (.getCenter map-obj) (-> new-data :center))
-    (.setCenter map-obj (-> new-data :center clj->js))))
+  [elem new-data]
+  (let [{:keys [map-obj map-data]} (get @maps elem)]
+
+    ;; Markers
+    (markers/update-markers! map-obj (:markers map-data) (:markers new-data))
+
+    ;; Directions
+    (when-not (= (-> map-data :directions) (-> new-data :directions))
+      (dir/set-directions! map-obj (:directions new-data)))
+
+    ;; The map itself (only center currently).
+    (when-not (loc/same-place? (.getCenter map-obj) (-> new-data :center))
+      (.setCenter map-obj (-> new-data :center clj->js)))
+
+    ;; Finally update the stored state.
+    (swap! maps assoc-in [elem :map-data] new-data)))
 
 (defn update-map!
   "Updates the map at elem in place to reflect the new data."
   [elem new-data]
   (when-let [{:keys [map-obj map-data] :as map} (get @maps elem)]
     (when (not= map-data new-data) 
-      (update-map* map new-data))))
+      (update-map* elem new-data))))
 
 (defn attach-map!
   "Creates a new map object attached to the given DOM element if one does not
@@ -37,8 +49,8 @@
   [elem map-data]
   (if (not (contains? @maps elem))
     (let [map-obj (google.maps.Map. elem (clj->js (init-args map-data)))]
-      (swap! maps assoc elem {:map-obj map-obj :map-data map-data})
-      (update-map* {:map-obj map-obj :map-data map-data} map-data))
+      (swap! maps assoc elem {:map-obj map-obj :map-data {}})
+      (update-map* elem map-data))
     (throw "Error! You are trying to attach a new map to a DOM element
     that already has one. Call detach-map! first, or use update-map!
     instead.")))
